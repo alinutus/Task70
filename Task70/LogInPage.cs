fc5f2a93993a21f636a71f8f9feef5e684c7257b
alinutus <a2.andreeva@gmail.com>
2022-03-28 00:40:15 +0200

    Add project files.

---
+++ Task70/LogInPage.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Task50
+{
+    public class LoginPage
+    {
+        private static readonly string URL = "https://yandex.ru";
+        private static readonly By LOGIN_BUTTON_FIRST = By.XPath("//a[@data-statlog='notifications.mail.logout.enter']");
+        private static readonly By LOGIN_NAME_TEXT_FIELD = By.XPath("//span/input[@type='text']");
+        private static readonly By LOGIN_BUTTON_SECOND = By.Id("passp:sign-in");
+        private static readonly By PASSWORD_TEXT_FIELD = By.Name("passwd");
+
+
+        protected IWebDriver _driver;
+
+        public LoginPage(IWebDriver driver)
+        {
+            _driver = driver;
+            driver.Url = URL;
+        }
+
+        public void LoginName(string name)
+        {
+            IWebElement loginButtonFirst = _driver.FindElement(LOGIN_BUTTON_FIRST);
+            loginButtonFirst.Click();
+
+            IWebElement loginNameTextField = _driver.FindElement(LOGIN_NAME_TEXT_FIELD);
+            loginNameTextField.SendKeys(name);
+
+            IWebElement loginButtonSecond = _driver.FindElement(LOGIN_BUTTON_SECOND);
+            loginButtonSecond.Click();
+
+            Thread.Sleep(1000); /* This is fluent waiter */
+        }
+
+        public LoginPage LoginPassword(string password)
+        {
+            IWebElement passwordTextField = _driver.FindElement(PASSWORD_TEXT_FIELD);
+            passwordTextField.SendKeys(password);
+
+            IWebElement loginButtonSecond = _driver.FindElement(LOGIN_BUTTON_SECOND);
+            loginButtonSecond.Click();
+
+            Thread.Sleep(1000);
+
+            return new LoginPage(_driver);
+        }
+    }
+}
---
+++ Task70/YandexLogInTest.cs
@@ -0,0 +1,50 @@
+using NUnit.Framework;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+
+namespace Task50
+{
+    public class YandexLoginTests
+    {
+        private static readonly By USERNAME_FIRST_LETTER = By.ClassName("username__first-letter");
+
+        private IWebDriver Driver;
+
+        [Test]
+        [TestCase("alinutus@yandex.ru", "coherent1")]
+        [TestCase("autolilia", "coherent1")]
+        public void LogIn(string name, string password)
+        {
+            Driver = new ChromeDriver();
+            Driver.Manage().Timeouts().ImplicitWait = System.TimeSpan.FromSeconds(15);
+
+            var loginPage = new LoginPage(Driver);
+
+            loginPage.LoginName(name);
+
+            loginPage.LoginPassword(password);
+
+            var wait = new WebDriverWait(Driver, System.TimeSpan.FromSeconds(15));
+            var element = wait.Until(condition =>
+            {
+                try
+                {
+                    var elementToBeDisplayed = Driver.FindElement(USERNAME_FIRST_LETTER);
+                    return elementToBeDisplayed.Text == "A" || elementToBeDisplayed.Text == "a";
+                }
+                catch (NoSuchElementException)
+                {
+                    return false;
+                }
+            }
+            );
+
+            var loginElement = Driver.FindElement(USERNAME_FIRST_LETTER).Text;
+            Assert.AreEqual("A", loginElement);
+        }
+    }
+}
